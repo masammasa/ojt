43a06b8a2f5230db8f96a0fe014ff4b8cf0adcdf
masammasa <afbteamx@gmail.com>
2022-03-15 16:47:08 +0900

    フォルダ構成変更済

--- index.php
+++ index.php
@@ -1,2 +1,144 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./reset.css">
+  <link rel="stylesheet" type="text/css"
+    href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+  <link rel="stylesheet" type="text/css"
+    href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/5-2-1/css/5-2-1.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="header">
+      <div class="openbtn1"><span></span><span></span><span></span></div>
+      <ul class="headerLink">
+        <li><a href="">Menu01</a></li>
+        <li><a href="">Menu02</a></li>
+        <li><a href="">Menu03</a></li>
+        <li><a href="">Menu04</a></li>
+      </ul>
+      <div class="spMenu">
+        <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="ロゴ">
+        <ul>
+          <li><a href="">Menu01</a></li>
+          <li><a href="">Menu02</a></li>
+          <li><a href="">Menu03</a></li>
+          <li><a href="">Menu04</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="keyvisual"></div>
+    <section id="Latest-Articles">
+      <h2>Latest Articles</h2>
+      <div class="image-lists">
+        <div class="contents">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_1.png" alt="" class="image">
+          <div class="word">
+            <p>2018/5/20</p>
+            <p>おしゃれカフェがありますよ</p>
+          </div>
+          <div class="read">
+            <p class="read-more">READ MORE</p>
+            <p class="read-line">ー</p>
+          </div>
+
+
+
+        </div>
+        <div class="contents">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_2.png" alt="" class="image">
+          <div class="word">
+            <p>2018/5/19</p>
+            <p>あのネオンはいつ交換するのか！？観覧車の謎に迫る！</p>
+          </div>
+          <div class="read">
+            <p class="read-more">READ MORE</p>
+            <p class="read-line">ー</p>
+          </div>
+
+
+        </div>
+
+        <div class="contents">
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_3.png" alt="" class="image">
+          <div class="word">
+            <p>2018/5/18</p>
+            <p>ラソナの社内はこんなのよ</p>
+          </div>
+          <div class="read">
+            <p class="read-more">READ MORE</p>
+            <p class="read-line">ー</p>
+          </div>
+
+        </div>
+
+        <div class="image-lists">
+          <div class="contents">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_4.png" alt="" class="image">
+            <div class="word">
+              <p>2018/5/27</p>
+              <p>お隣のアラハはハワイ？</p>
+            </div>
+
+            <div class="read">
+              <p class="read-more">READ MORE</p>
+              <p class="read-line">ー</p>
+            </div>
+
+          </div>
+
+          <div class="contents">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_5.png" alt="" class="image">
+            <div class="word">
+              <p>2018/5/16</p>
+              <p>なぜテント？ラソナの人に聞いてみた</p>
+            </div>
+            <div class="read">
+              <p class="read-more">READ MORE</p>
+              <p class="read-line">ー</p>
+            </div>
+
+          </div>
+
+          <div class="contents">
+            <img src="<?php echo get_template_directory_uri();?>/assets/img/post_img_6.png" alt="" class="image">
+            <div class="word">
+              <p>2018/5/15</p>
+              <p>ベイエリアおしゃれすぎる問題</p>
+
+            </div>
+            <div class="read">
+              <p class="read-more">READ MORE</p>
+              <p class="read-line">ー</p>
+            </div>
+
+
+
+          </div>
+
+        </div>
+    </section>
+
+
+
+  </main>
+  <footer>
+    <p>Copyright &copy; Shinohara. ALL RIGHTS RESERVED.</p>
+  </footer>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/5-2-1/js/5-2-1.js"></script>
+  <script src="<?php echo get_template_directory_uri();?>/masa.js"></script>
+</body>
+
+</html>
