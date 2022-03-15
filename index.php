7f1a27467629765e7ce0503744737decc886101f
masammasa <afbteamx@gmail.com>
2022-03-15 17:55:07 +0900

    no message

--- index.php
+++ index.php
@@ -5,11 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./reset.css">
-  <link rel="stylesheet" type="text/css"
-    href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
-  <link rel="stylesheet" type="text/css"
-    href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/5-2-1/css/5-2-1.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/reset.css">
+  <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+  <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/5-2-1/css/5-2-1.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
   <title>Document</title>
 </head>
